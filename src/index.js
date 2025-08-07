5da62cc059c6d91a6b37a6ab1ca6d3de5d7dac74
MatheusC08 <matheuscuerbas@gmail.com>
2025-08-07 17:20:56 -0300

    Projeto Finalizado

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
-console.log('Hello, World!');
+//informações e atributos do heroi
+let nomeHeroi = "Zhè";
+
+//Função para calcular Saldo de vitórias
+function nivelRanqueado(vitorias, derrotas) {
+    let nivelRanqueado = 120 - 19;
+    return nivelRanqueado;
+}
+
+//Calculando saldo de vitórias
+let vitorias = 120;
+let derrotas = 19;
+let saldoVitorias = nivelRanqueado(vitorias, derrotas);
+
+//Verificando nivel do heroi
+if (nivelRanqueado < 10) {
+    nivelHeroi = "Ferro";
+} else if (nivelRanqueado >= 11 && nivelRanqueado < 20) {
+    nivelHeroi = "Bronze";
+} else if (nivelRanqueado >= 21 && nivelRanqueado < 50) {
+    nivelHeroi = "Prata";
+} else if (nivelRanqueado >= 51 && nivelRanqueado < 80) {
+    nivelHeroi = "Ouro";
+} else if (nivelRanqueado >= 81 && nivelRanqueado < 90) {
+    nivelHeroi = "Diamante";
+} else if (nivelRanqueado >= 91 && nivelRanqueado < 100) {
+    nivelHeroi = "Lendário";
+} else {
+    nivelHeroi = "Imortal";
+}
+
+//Mensagem de saída
+console.log("O Herói " + nomeHeroi + " tem de saldo de " + saldoVitorias + " vitórias e está no nível " + nivelHeroi);
